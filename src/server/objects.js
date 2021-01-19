0ce5e5d47b002dc18e7a820bd1fabdb345dd98de
willhey <mike@willhey.co.uk>
2021-01-19 08:38:56 +0000

    inventory bugs fixed - still issue with different caseQty

--- src/server/objects.js
+++ src/server/objects.js
@@ -20,42 +20,57 @@ const objects = {
         }
     ],
     growth: 3,
+    casesPerPalletInboundMixed : 28,
+    casesPerPalletOutbound : 18,
+    pcentFullCaseFromActive : 0.2,
+    vasStationsBase : 40,  //current divided by 2 shifts
+    curInv : 1000000,
+    invFullPalletPcent : 0.78,
+    invPairsPercase : 6.5,
+    invFullPallUtil : 0.7,
+    CurPandHoldPallets : 950,
     periods: [1, 2, 3, 4, 5, 6, 7],
     flowChart: `flowchart LR
     subgraph Dock
-    Unload(Unload <BR>100 containers)
-    Mixed_SKU_Pallet[300 cases on to <BR>mixed SKU pallets]
-    Single_SKU_Pallet[200 cases on to <BR>single SKU Pallet]
+    Unload("Unload {D1} <BR>{containersUnload} containers")
+    Mixed_SKU_Pallet[max {maxCasesUnloadMixed} cases<BR>avg {avgCasesUnloadMixed} cases<BR>on to mixed SKU pallets]
+    Single_SKU_Pallet[max {maxCasesUnloadSingle} cases <BR>avg {avgCasesUnloadSingle} cases<BR> on to single SKU Pallet]
     Unload-->Single_SKU_Pallet
     Unload-->Mixed_SKU_Pallet
     end
-    subgraph PalletPackTop
+    subgraph PalletRackMiddle
+    MidRack-->|moves from middle included in moves from top|a
+    end
+    subgraph PalletRackTop
+    TopRack["All Pallet Racking<BR><BR>All pallets<BR>including middle<BR>{invFullPalls}"]
     Retrieve
     Putaway
     end
-    subgraph PalletRackMiddle
-    Rack
-    end
     subgraph Active
+    Shelving["Shelving  {D5}<BR>Store {invCartons} cartons"]
     PickStation
+    FullCasePick
     Robots
-    Replenish
+    Replenish["Replenish<BR>{[fcaTxt0] D6 + D5}<BR>max {maxCaseReplenActive} cases<BR>avg {avgCaseReplenActive} cases"]
     end
-    subgraph Finish
-    KEY
-    VAS
+    subgraph Finish["Finish     Non Active {D6}"]
+    KEY["KEY {D7}<BR> max {maxKeyCartons} cartons {maxKeyUnits} units<BR> max {avgKeyCartons} cartons {avgKeyUnits} units"]
+    VAS["VAS <BR>{vasStations} Workstations"]
     end
     subgraph Loading
-    Hold--->Load
+    Hold["Hold<BR>{PandHoldPallets} pallets"]--->Load
     end
-    Single_SKU_Pallet-->|{PALLETS_PLACEHOLDER} pallets|Putaway
-    Retrieve-->|600 pallets|Replenish
-    Mixed_SKU_Pallet-->|500 pallets|Replenish
-    Retrieve-->|700 pallets|Finish
-    PickStation-->Finish
-    VAS-->Hold
-    Rack-->Replenish
-    Retrieve-->|Non peak|Rack`
+
+    Retrieve-->|Non peak|MidRack
+    Single_SKU_Pallet-->|" {D2}<BR>max {maxPalletsUnloadSingle} pallets<BR>avg {avgPalletsUnloadSingle} pallets"|Putaway
+
+    Retrieve-->|"{[fcaTxt1] D6 + D5 - D3}<BR>max {maxPalletsReplenActive} pallets<BR>avg {avgPalletsReplenActive} pallets"|Replenish
+
+    Mixed_SKU_Pallet-->|"{D3}<BR>max {maxPalletsUnloadMixed} pallets<BR>avg {avgPalletsUnloadMixed} pallets"|Replenish
+    Retrieve-->|"{[fcaTxtNot] {D6}<BR>max {maxFCCasePalletToFinish} cartons<BR>avg {avgFCCasePalletToFinish} cartons"|Finish
+    FullCasePick-->|"{[fcaTxt2] {D6}<BR>max {maxFCCaseActiveToFinish} cartons<BR>avg {avgFCCaseActiveToFinish} cartons"|Finish
+    PickStation-->|"{D5}<BR>max {maxCartonsPicked} cartons<BR>avg {avgCartonsPicked} cartons"|Finish
+    Finish-->|"{D8}<BR>max {maxShipCartons} cartons<BR>max {maxShipPallets} pallets<BR>avg {avgShipCartons} cartons<BR>avg {avgShipPallets} pallets"|Hold`
 }
 
 export default objects;
--- src/server/phases/process.js
+++ src/server/phases/process.js
@@ -4,13 +4,87 @@ import executeQuery from '../sql/executeQuery.js'
 import objects from '../objects.js'
 
 const process = async () => {
-    const query = `SELECT receivedDate, sum(pallets) as pallets FROM sasn GROUP BY 1 ORDER BY 2 DESC LIMIT 1;`
-    const data = await executeQuery('runQuery', query);
-    console.log(data[0]);
+    const replacers = {}
 
-    const pallets = data[0].pallets * objects.growth;
+    replacers.fcaTxt0 = (objects.pcentFullCaseFromActive * 100) + '%'
+    replacers.fcaTxt1 = replacers.fcaTxt0
+    replacers.fcaTxt2 = replacers.fcaTxt0
+    replacers.fcaTxtNot = ((1-objects.pcentFullCaseFromActive) * 100) + '%'
 
-    const flowChart = objects.flowChart.replace('{PALLETS_PLACEHOLDER}', pallets.toString());
+    const query1 = `SELECT receivedDate, COUNT(DISTINCT sa.shipment) as containers FROM sasn sa 
+    JOIN ( select shipment, SUM(cases) AS scases FROM sasn group BY 1 having scases > 10
+    ) lg ON lg.shipment = sa.shipment GROUP BY 1 ORDER BY 2 DESC LIMIT 1;`
+    const data1 = await executeQuery('runQuery', query1);
+    replacers.containersUnload = data1[0].containers 
+
+    const query2 = `select avg(pallets) AS avgPallets, max(pallets) AS maxPallets, avg(cases) as avgCases, max(cases) as maxCases FROM (SELECT receivedDate, sum(pallets) AS pallets, sum(cases) AS cases FROM sasn where pallets > 1 AND receivedDate < '20200401' GROUP BY 1) aa;`
+    const data2 = await executeQuery('runQuery', query2);
+    replacers.maxCasesUnloadSingle = data2[0].maxCases 
+    replacers.avgCasesUnloadSingle = data2[0].avgCases 
+    replacers.maxPalletsUnloadSingle = data2[0].maxPallets 
+    replacers.avgPalletsUnloadSingle = data2[0].avgPallets 
+
+    const query3 = `select max(cases) AS maxCases, AVG(cases) AS avgCases FROM (SELECT receivedDate, sum(cases) as cases FROM sasn where pallets <= 1 AND receivedDate < '20200401' GROUP BY 1) aa;`
+    const data3 = await executeQuery('runQuery', query3);
+    replacers.maxCasesUnloadMixed = data3[0].maxCases 
+    replacers.avgCasesUnloadMixed = data3[0].avgCases 
+    replacers.maxPalletsUnloadMixed = Math.ceil(data3[0].maxCases / objects.casesPerPalletInboundMixed)
+    replacers.avgPalletsUnloadMixed = Math.ceil(data3[0].avgCases / objects.casesPerPalletInboundMixed)
+
+    const query5 = `select avg(cartons) as avgCartons, max(cartons) as maxCartons, AVG(caseEquiv) as avgCaseEquiv, MAX(caseEquiv) as maxCaseEquiv FROM 
+    (SELECT LEFT(wave, 8), COUNT(*) as cartons, SUM(units/6) as caseEquiv FROM cartons WHERE cartonType = 'Active'  and wave < '20200401' GROUP BY 1)aa;`
+    const data5 = await executeQuery('runQuery', query5);
+    replacers.maxCartonsPicked = data5[0].maxCartons 
+    replacers.avgCartonsPicked = data5[0].avgCartons 
+
+//moves from pallet store are caseEquiv from carton less cases from mixed pallet receipts
+    const avgPalStoreMoves = (data5[0].avgCaseEquiv - data3[0].avgCases) / objects.casesPerPalletInboundMixed
+    const maxPalStoreMoves = (data5[0].maxCaseEquiv - data3[0].maxCases) / objects.casesPerPalletInboundMixed
+    replacers.avgPalletsReplenActive = Math.ceil(avgPalStoreMoves) 
+    replacers.maxPalletsReplenActive = Math.ceil(maxPalStoreMoves) 
+
+    const query6 = `select avg(cartons) as avgCartons, max(cartons) as maxCartons FROM 
+        (SELECT LEFT(wave, 8), COUNT(*) as cartons FROM cartons WHERE cartonType != 'Active'  and wave < '20200401' GROUP BY 1)aa;`
+    const data6 = await executeQuery('runQuery', query6);
+    replacers.maxFCCasePalletToFinish = Math.ceil(data6[0].maxCartons * (1 - objects.pcentFullCaseFromActive))
+    replacers.avgFCCasePalletToFinish = Math.ceil(data6[0].avgCartons * (1 - objects.pcentFullCaseFromActive))
+    replacers.maxFCCaseActiveToFinish = Math.ceil(data6[0].maxCartons * objects.pcentFullCaseFromActive)
+    replacers.avgFCCaseActiveToFinish = Math.ceil(data6[0].avgCartons * objects.pcentFullCaseFromActive)
+
+    replacers.maxCaseReplenActive = data5[0].maxCaseEquiv + replacers.maxFCCaseActiveToFinish
+    replacers.avgCaseReplenActive = data5[0].avgCaseEquiv + replacers.avgFCCaseActiveToFinish
+    
+    const query7 = `select avg(cartons) as avgCartons, max(cartons) as maxCartons, AVG(units) as avgUnits, max(units) as maxUnits, AVG(lnes) as avgLines, max(lnes) as maxLines  FROM 
+    (SELECT LEFT(wave, 8), COUNT(*) as cartons, SUM(units) as units, SUM(LINES2) AS LNES FROM cartons WHERE cartonType = 'KEY'  and wave < '20200601' GROUP BY 1)aa;`
+    const data7 = await executeQuery('runQuery', query7);
+    replacers.avgKeyCartons = data7[0].avgCartons
+    replacers.maxKeyCartons = data7[0].maxCartons
+    replacers.avgKeyUnits = data7[0].avgUnits
+    replacers.maxKeyUnits = data7[0].maxUnits
+
+    replacers.vasStations = objects.vasStationsBase
+
+    const query8 = `select avg(cartons) as avgCartons, max(cartons) as maxCartons, AVG(units) as avgUnits, max(units) as maxUnits  FROM 
+    (SELECT LEFT(wave, 8), COUNT(*) as cartons, SUM(units) as units, SUM(LINES2) AS LNES FROM cartons WHERE wave < '20200601' GROUP BY 1)aa;`
+    const data8 = await executeQuery('runQuery', query8);
+    replacers.avgShipCartons = data8[0].avgCartons
+    replacers.maxShipCartons = data8[0].maxCartons
+    replacers.avgShipPallets = data8[0].avgCartons / objects.casesPerPalletOutbound
+    replacers.maxShipPallets = data8[0].maxCartons / objects.casesPerPalletOutbound
+
+    replacers.invFullPalls = objects.curInv * objects.invFullPalletPcent / objects.casesPerPalletInboundMixed / objects.invPairsPercase / objects.invFullPallUtil
+    replacers.invCartons = objects.curInv * (1 - objects.invFullPalletPcent) / objects.invPairsPercase 
+
+    replacers.PandHoldPallets = objects.CurPandHoldPallets
+
+    let flowChart = objects.flowChart
+    Object.keys(replacers).forEach((key) => {
+        flowChart = flowChart.replace('{' + key + '}', Math.ceil(replacers[key]* objects.growth).toLocaleString());
+    })
+    Object.keys(replacers).forEach((key) => {
+        flowChart = flowChart.replace('[' + key + ']', replacers[key]);
+    })
+
     let flowChartUpdate = [];
     flowChartUpdate.push({
         value: flowChart
